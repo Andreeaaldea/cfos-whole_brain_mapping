c02f9d7cb6aaeb801bebc3ac4639dedb5351c675
Andy A <78367418+Andreeaaldea@users.noreply.github.com>
2025-08-20 11:53:39 +0100

    heatmap 3d

--- heatmap_3D.py
+++ heatmap_3D.py
@@ -1,10 +1,14 @@
 import brainglobe_heatmap as bgh
-
 import os
 import pandas as pd
-
-os.chdir("E:\\Andy\\3dplots")
-file_path = 'CBTC.csv'
+# Example CSV file path
+os.chdir("Y:\public\projects\AnAl_20240405_Neuromod_PE\PE_mapping\processed_data")
+file_path = 'effect_sizes_PE_vs_CT_by_hemi_with_categories.xlsx'
+dd = pd.read_excel(file_path, sheet_name='WT_L_none', usecols=['acronym', 'g'])
+df = pd.DataFrame(dd)
+dict_without_headers = dict(zip(df["acronym"], df["g"]))
+val_only = df.values
+all_areas = df.to_dict()
 
 # Read specific columns (e.g., 'Column1' and 'Column3')
 columns_to_import = ['Acronym', 'dNormcohen_d']
@@ -19,10 +23,10 @@ scene = bgh.Heatmap(
     orientation="sagittal",  # Change to 'sagittal', 'horizontal', or a tuple (x,y,z)
     thickness=500,  # Thickness of the section
     title="3D Heatmap - Frontal Orientation",
-    vmin=-2,  # Minimum value for colormap
-    vmax=2,  # Maximum value for colormap
+    vmin=-1.75,  # Minimum value for colormap
+    vmax=1.75,  # Maximum value for colormap
     format="3D",  # 3D format for rendering
-    cmap="RdBu"  # Color map for visualization
+    cmap="coolwarm"  # Color map for visualization
 )
 
 # Display the 3D scene
